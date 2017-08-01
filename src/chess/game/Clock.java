b9efc8bc410cda9b431c77f1a119bc4d34c2f93a
Rahul <haxdds@gmail.com>
2017-07-31 22:43:52 -0400

    updated clock and moveLog

--- src/chess/game/Clock.java
+++ src/chess/game/Clock.java
@@ -1,7 +1,121 @@
 package chess.game;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Rahul on 7/31/2017.
  */
-public class Clock {
+public class Clock{
+    /**
+     *
+     */
+    private final int length;
+    private final boolean incremented;
+    private int increment;
+    private int time;
+    private Timer timer;
+
+    /**
+     *
+     * @param length
+     * @param increments
+     */
+    public Clock(int length, boolean increments){
+        this.length = length;
+        this.incremented = increments;
+        this.time = length;
+        this.timer = new Timer();
+    }
+
+    /**
+     *
+     * @param length
+     */
+    public void setIncrement(int length){
+        if(!isIncremented()) throw new IllegalArgumentException("Clock must be an incremented type");
+        this.increment = length;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isIncremented(){return this.incremented;}
+
+    /**
+     *
+     */
+    public void incrementTime(){
+        if(!isIncremented()) throw new IllegalArgumentException("Clock must be an incremented type");
+        time += increment;
+    }
+
+    /**
+     *
+     */
+    public void start(){
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                update();
+            }
+        }, 1000);
+    }
+
+    /**
+     *
+     */
+    public void stop(){
+        timer.cancel();
+        timer.purge();
+    }
+
+    /**
+     *
+     */
+    public void update(){
+        this.time--;
+        if(time <= 0) {
+            stop();
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getTime(){
+        return this.time;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getLength(){
+        return this.length;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getIncrement(){
+        return this.increment;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String toString(){
+        return "Time Left: " + this.time;
+    }
+
+
+
+
+
+
 }
--- src/chess/game/MoveLog.java
+++ src/chess/game/MoveLog.java
@@ -1,7 +1,103 @@
 package chess.game;
 
+import chess.structure.Move;
+import chess.structure.Square;
+
+import java.util.ArrayList;
+
 /**
  * Created by Rahul on 7/31/2017.
  */
 public class MoveLog {
+    /**
+     *
+     */
+    private ArrayList<Move[]> moveLog = new ArrayList<>();
+
+    /**
+     *
+     * @param game
+     */
+    public MoveLog(Game game){
+
+    }
+
+    /**
+     *
+     * @param moveNumber
+     * @return
+     */
+    public Move[] getMove(int moveNumber){
+        return moveLog.get(moveNumber - 1);
+    }
+
+    /**
+     *
+     * @param moveNumber
+     * @return
+     */
+    public Move getWhiteMove(int moveNumber){
+        return getMove(moveNumber)[0];
+    }
+
+    /**
+     *
+     * @param moveNumber
+     * @return
+     */
+    public Move getBlackMove(int moveNumber){
+        return getMove(moveNumber)[1];
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Move[] getLastMove(){
+        return moveLog.get(moveLog.size() - 1);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Move getWhiteLastMove(){
+        return getLastMove()[0];
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Move getBlackLastMove(){
+        return getLastMove()[1];
+    }
+
+    /**
+     *
+     * @param move
+     * @return
+     */
+    public boolean containsMove(Move move){
+        return false;
+    }
+
+    /**
+     *
+     * @param square
+     * @return
+     */
+    public boolean containsSquare(Square square){
+        return false;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String toString(){
+        return null;
+    }
+
+
 }
