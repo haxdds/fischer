18656d4eb6e7068eb993c7c5ddfff4078fe1d41f
Rahul <haxdds@gmail.com>
2020-06-04 14:30:44 -0700

    Added PieceSet class to replace HashSet<Piece>

--- src/chess/structure/PieceSet.java
+++ src/chess/structure/PieceSet.java
@@ -1,4 +1,32 @@
 package chess.structure;
 
-public class PieceSet {
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class PieceSet implements Iterable<Piece>{
+
+    private HashSet<Piece> pieces;
+
+    public PieceSet(HashSet<Piece> pieces){
+        this.pieces = pieces;
+    }
+
+    public PieceSet(){this.pieces = new HashSet<Piece>();}
+
+    public void add(Piece p){
+        pieces.add(p);
+    }
+
+    public void remove(Piece p){
+        pieces.remove(p);
+    }
+
+    public boolean hasPiece(Piece p){
+        return pieces.contains(p);
+    }
+
+    @Override
+    public Iterator<Piece> iterator() {
+        return pieces.iterator();
+    }
 }
