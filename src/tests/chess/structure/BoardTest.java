d5a5086174aa809e1307aa9d315f17994097e1d4
haxdds <haxdds@gmail.com>
2020-06-09 22:25:15 -0700

    Test class for Board.java

---
+++ src/tests/chess/structure/BoardTest.java
@@ -0,0 +1,20 @@
+package chess.structure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    void movePiece() {
+    }
+
+    @Test
+    void castle() {
+    }
+
+    @Test
+    void enPassante() {
+    }
+}
