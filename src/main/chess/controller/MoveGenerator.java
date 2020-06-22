2578882f39ab7a814d734df9f684303bc9622725
haxdds <haxdds@gmail.com>
2020-06-22 00:29:30 -0700

    refactor pawn iterate

--- src/main/chess/controller/MoveGenerator.java
+++ src/main/chess/controller/MoveGenerator.java
@@ -51,7 +51,7 @@ public class MoveGenerator {
      * @return whether that translation is valid on that board
      * @see Translation#getSignature()
      */
-    public boolean checkTranslation(Board board, Translation t, Square start,int[] signature) {
+    public boolean checkTranslation(Board board, Translation t, Square start, int[] signature) {
         if (start.translate(t).inBounds()) {
             Square s = board.translateSquare(start, t);
             if (!t.isSignature(signature) || start.getPiece().getType() == Type.KNIGHT) {
@@ -76,18 +76,15 @@ public class MoveGenerator {
      *
      * @param start the square the pawn is starting on
      * @param board the board the game is being played on
-     * @param startingRow the starting row of the pawn at the
-     *                    beginning of the game
-     * @param y the direction the pawn is supposed to move in
-     *          (1 for white and -1 for black)
      * @return the list of squares the pawn can move to
-     * @see MoveGenerator#checkPawnTranslation(Translation, Square, Board, int, int)
+     * @see MoveGenerator#checkPawnTranslation(Board, Square,Translation)
      * @see MoveGenerator#getEnPassanteMoves(Board, Square)
      */
-    public ArrayList<Square> pawnIterate(Board board, Square start,int startingRow, int y){
+    public ArrayList<Square> pawnIterate(Board board, Square start){
         ArrayList<Square> moves = new ArrayList<>();
         for (Translation t : Type.PAWN.getGroup()) {
-            if(checkPawnTranslation(t, start, board, y, startingRow)){
+            if(!start.translate(t).inBounds()) continue;
+            if(checkPawnTranslation(board, start, t)){
                 moves.add(board.translateSquare(start, t));
             }
         }
@@ -97,24 +94,6 @@ public class MoveGenerator {
         return moves;
     }
 
-    /**
-     * Retrieves the list of all possible moves that can be
-     * made by a pawn from the given square on the given board.
-     * @param start the square which houses the pawn
-     * @param board the board on which the game is being played
-     * @return the list of all possible moves for the pawn
-     */
-    public ArrayList<Square> pawnIterate(Board board, Square start) {
-        if(start.getPiece().getColor() == Color.BLACK){
-            return pawnIterate(board, start, 6,-1);
-        }else{
-            return pawnIterate(board, start, 1, 1);
-        }
-    }
-
-
-
-
 
     /**
      * TODO: MAKE THIS PAWN TRANSLATION CHECKING METHOD EASIER TO UNDERSTAND!
@@ -122,66 +101,72 @@ public class MoveGenerator {
 
     /**
      *
-     * @param t the translation being checked
-     * @param start the square the pawn is starting from
      * @param board the board the game is being played on
-     * @param y the direction the pawn is supposed to move in (1 for white
-     *          and -1 for black)
-     * @param startingRow the row that the pawn is placed at the beginning of the
-     *                    game
+     * @param start the square the pawn is starting from
+     * @param t the translation being checked
      * @return whether that translation is valid for the pawn
      */
-    public boolean checkPawnTranslation(Translation t, Square start, Board board, int y, int startingRow){
-        if (t.getY() * y < 0) return false; //if move is in opposite direction
-        if (start.translate(t).inBounds()) {
-            Square s = board.translateSquare(start, t);
-            if ((t.getY() == y && t.getX() == -1) || (t.getY() == y && t.getX() == 1)) {
-                if (!s.isOccupied()) return false;
-                if (s.getPiece().getColor() == start.getPiece().getColor()) return false;
-                return true;
-            } else if (t.getY() == (2 * y)) {
-                if (start.getRow() == startingRow) {
-                    if (!s.isOccupied()) {
-                        return true;
-                    }
-                }
-            } else {
-                if (!s.isOccupied()) {
-                    return true;
-                }
+    public boolean checkPawnTranslation(Board board, Square start, Translation t){
+        Square end = board.translateSquare(start, t);
+        // row pawn is initialized
+        int startingRow = start.getPiece().getColor() == Color.WHITE? 1 : 6;
+        // correct y signature of given pawn
+        int y = start.getPiece().getColor() == Color.WHITE? 1 : -1;
+
+        // not correct direction
+        if(t.getSignature()[1] != y) return false;
+
+        // if 1 square move
+        if(t.getY() == 1 * y){
+            // if also lateral
+            if(t.getX() == 1 || t.getX() == -1){
+                if(!end.isOccupied()) return false;
+                if(end.getPiece().getColor() == start.getPiece().getColor()) return false;
+            }else{
+                if(end.isOccupied()) return false;
             }
         }
-        return false;
+
+        // if 2 square move
+        if(t.getY() == 2 * y){
+            if(start.getRow() != startingRow) return false;
+            if(end.isOccupied()) return false;
+        }
+
+        return true;
     }
+
 
 
     /**
      *
-     * @param row the row on which the pawn is on
-     * @param y the direction the pawn moves in (1 for white, -1 for black)
-     * @return whether the pawn can move en passante
-     * @see Board#getLastMove()
+     * @param start the square which the pawn in question is on
+     * @param last the last move on the board
+     * @return whether the pawn on the given square can en passante     *
      */
-    public boolean canEnPassante(int col, int row, int y, Move last){
-        if(row != 4 && row != 3) return false;
-        if(row == 4 && y != 1) return false;
-        if(row == 3 && y != -1) return false;
-        //System.out.println("last: " + last);
-        //System.out.println("piece: " + last.getStart().getPiece().toString());
+    public boolean canEnPassante(Square start, Move last){
+        // if first move of game
+        if(last == null) return false;
+        // if last move is not a pawn move
         if(last.getEnd().getPiece().getType() != Type.PAWN) return false;
-        int deltaCol =  Math.abs(col - last.getStart().getCol());
-        if(deltaCol > 1) return false;
-        if(deltaCol == 0) return false;
-        if(row == 4){
-            if(last.getEnd().getPiece().getColor() == Color.WHITE) return false;
-            if(last.getEnd() == null) return false;
-            if(last.getStart().getRow() != 6 && last.getEnd().getRow() != 4) return false;
+        // if last pawn move and current pawn move are not adjacent
+        if(Math.abs(start.getCol() - last.getStart().getCol()) != 1) return false;
+
+        int row = start.getRow();
+        // if pawn in question is white
+        if(start.getPiece().getColor() == Color.WHITE){
+            // white pawn must be on 5th rank
+            if(row != 4) return false;
+            // black pawn must have moved 2 squares from 7th to 5th rank
+            if(last.getStart().getRow() != 6 || last.getEnd().getRow() != 4) return false;
+        }else{
+            // black pawn must be on 4th rank
+            if(row != 3) return false;
+            // white pawn must have moved 2 squares from 2nd to 4th rank
+            if(last.getStart().getRow() != 1 || last.getEnd().getRow() != 3) return false;
         }
-        if(row == 3){
-            if(last.getEnd().getPiece().getColor() == Color.BLACK) return false;
-            if(last.getEnd() == null) return false;
-            if(last.getStart().getRow() != 1 && last.getEnd().getRow() != 3) return false;
-        }
+
+        // if all conditions passed
         return true;
     }
 
@@ -191,26 +176,18 @@ public class MoveGenerator {
      * @param board the board the game is being played on
      * @return the square the pawn can en passante to. Null is returned
      * if the pawn cannot en passante.
-     * @see MoveGenerator#canEnPassante(int, int, int, Move)
+     * @see MoveGenerator#canEnPassante(Square, Move)
      */
     public ArrayList<Square> getEnPassanteMoves(Board board, Square start){
         ArrayList<Square> enpassantMoves = new ArrayList<>();
-        int row = start.getRow();
-        int y;
-        if(start.getPiece().getColor() == Color.WHITE){
-            y = 1;
-        }else{
-            y = -1;
-        }
         Move last = board.getLastMove();
-        System.out.println(last + "\n=====" + board);
-        if(canEnPassante(start.getCol(), row, y, last)){
-            if(row == 4){
-                enpassantMoves.add(board.getSquare(5, last.getEnd().getCol()));
-            }
-            if(row == 3){
-                enpassantMoves.add(board.getSquare(2, last.getEnd().getCol()));
-            }
+        // if can en passant
+        if(canEnPassante(start, last)){
+            // move on to the same column
+            int endCol = last.getEnd().getCol();
+            // move between last pawn move start row and end row
+            int endRow = (last.getStart().getRow() + last.getEnd().getRow())/2;
+            enpassantMoves.add(board.getSquare(endRow, endCol));
         }
         return enpassantMoves;
     }
